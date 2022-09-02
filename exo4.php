124fe2d23191d35cdd53de432c0ddcf9b69c2011
Baimenov Timour <buchukiri@gmail.com>
2022-09-02 09:51:07 +0200

    correction exos 4

--- exo4.php
+++ exo4.php
@@ -36,6 +36,24 @@ $arrayB = [85, "toi", 95, "la", 65, 94, 85, "avec", 37, "chat"];
             <h2 class="exercice-ttl">Question 1</h2>
             <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau et retourne la chaîne de caractère HTML permettant d'afficher les valeurs du tableau sous la forme d'une liste.</p>
             <div class="exercice-sandbox">
+            <?php
+
+
+                function getHtmlFromArray(array $array) :string {
+                    $list ="";
+                    foreach($array as $index => $value) {
+                        $list .= "<li>$index: $$value</li>"
+                    }
+                    return "<ul>$list</ul>"
+                }
+
+                echo getHtmlFromArray($arrayA);
+
+
+            ?>
+
+           
+
 
             </div>
         </section>
@@ -46,6 +64,35 @@ $arrayB = [85, "toi", 95, "la", 65, 94, 85, "avec", 37, "chat"];
             <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau d'entiers et retourne uniquement les valeurs paires. Afficher les valeurs du tableau sous la forme d'une liste HTML.</p>
             <div class="exercice-sandbox">
 
+            <?php
+            
+            function getNumberPair(array $array):string {
+                $list = "<ul>";
+                foreach($array as $number) {
+                    if ($number%2 ===0) {
+                        $list .= "<li>".$number."</li>"
+                    }
+                }
+                $list .="</ul>";
+                return $list;
+            }
+            
+            echo getNumberPair($arrayA)
+
+
+            function getEvenNumberFromArray(array $array) :array {
+                // $a =[];
+                // foreach($array as $number) {
+                //     if($number%2 ===0) {
+                //         $a[] = $number
+                //     }
+                //     return $a
+                return array_filter($array,fn($v) => $v%2 === 0);
+            }
+
+            echo getHtmlFromArray(getEvenNumberFromArray($array))
+            ?>
+
             </div>
         </section>
 
@@ -54,6 +101,14 @@ $arrayB = [85, "toi", 95, "la", 65, 94, 85, "avec", 37, "chat"];
             <h2 class="exercice-ttl">Question 3</h2>
             <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau d'entiers et retourne uniquement les entiers d'index pair</p>
             <div class="exercice-sandbox">
+
+            <?php
+            
+            function getEvenInex(array $a):array {
+                return array_filter($a,fn($k) => $k%2 === 0)
+            }
+            
+            ?>
 
             </div>
         </section>
@@ -64,6 +119,30 @@ $arrayB = [85, "toi", 95, "la", 65, 94, 85, "avec", 37, "chat"];
             <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau d'entiers. La fonction doit retourner les valeurs du tableau mulipliées par 2.</p>
             <div class="exercice-sandbox">
 
+
+            <?php
+            
+            /**
+             * Filter numeric values of the array and multiply them by 2.
+             * 
+             * @param array $array
+             * @return array
+             */
+
+            function getValueMultiplyBy2(array $array):array {
+                $newArray = [];
+                foreach ($array as $value) {
+                    if(is_numeric($value)) $newArray[] = $value * 2;
+                }
+                return $newArray
+            }
+
+            var_dump(getValueMultiplyBy2($arrayA))
+            
+            ?>
+
+
+
             </div>
         </section>
 
@@ -72,6 +151,28 @@ $arrayB = [85, "toi", 95, "la", 65, 94, 85, "avec", 37, "chat"];
             <h2 class="exercice-ttl">Question 4</h2>
             <p class="exercice-txt">Déclarer une fonction qui prend en paramètre un tableau d'entiers et un entier. La fonction doit retourner les valeurs du tableau divisées par le second paramètre</p>
             <div class="exercice-sandbox">
+
+            
+            <?php
+            
+            /**
+             * Filter numeric values of the array and divided by the seconde parametre
+             * 
+             * @param array $array The list of values to be divided
+             * @param integer $div The divider
+             * @return array
+             */
+
+            function divideValuesBy(array $array, int $div):array {
+                    return array_map(fn($v) =>$v/$div, array_filter($array, "is_numeric"))
+                return $newArray
+            }
+
+            var_dump(getValueMultiplyBy2($arrayA))
+            
+            ?>
+
+            
                 
             </div>
         </section>
